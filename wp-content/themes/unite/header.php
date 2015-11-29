0fe1d4c809aca07964072c85b1389e9a5ccdcbe4
Yi Yang <git@iyyang.com>
2015-11-29 13:21:53 +0800

    Updated style.

--- wp-content/themes/unite/header.php
+++ wp-content/themes/unite/header.php
@@ -26,7 +26,7 @@
 			<?php do_action( 'before' ); ?>
 			<header id="masthead" class="site-header col-sm-12" role="banner">
 
-				<div class="site-branding col-md-6">
+				<div class="site-branding col-md-8 col-md-offset-2">
 					<?php if( get_header_image() != '' ) : ?>
 
 						<div id="logo">
